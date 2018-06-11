0647a53841bc986a60874825b308e589829b3060
Jit <sarbojit.project@gmail.com>
2018-06-11 10:43:58 -0700

    Entering environment stuff.

---
+++ config/keys.js
@@ -0,0 +1,20 @@
+/*
+**  Module: Config
+**  Author: Sarbojit Mukherjee (Jit)
+**          jit@baanda.com
+**  Comments: This figures  out the environment (prod, QA, Dev etc.)
+**   
+**  Rivision History:
+**
+**  Date           Author     Description
+**  06/11/2018       Jit      Setting up the preliminary logic
+*/
+if (process.env.NODE_ENV === 'production'){
+    // we are in production -- return 
+    module.exoprts = require('./prod');  // prod keys are in prod.js 
+} else {
+    // we are in development and return the dev keys.
+    // Here we can opt to select local environment variables to check and setup other
+    // environment as QA, Staging etc.
+    modules.exports = require ('./dev');  // This would export dev.js to be used by app.js   
+}
